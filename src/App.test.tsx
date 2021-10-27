acd1192862728269603a5a7c6c22a2dd01792819
David Christie <davidchristie@users.noreply.github.com>
2021-10-27 19:32:59 +1300

    Initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import { PassThrough } from "stream";
+import { App } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  const originalNavigator = window.navigator;
+
+  let mockGetUserMedia: jest.Mock;
+
+  beforeAll(() => {
+    mockGetUserMedia = jest.fn();
+    Object.defineProperty(window.navigator, "mediaDevices", {
+      value: {
+        getUserMedia: mockGetUserMedia,
+      },
+    });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(window, "navigator", {
+      value: originalNavigator,
+    });
+  });
+
+  describe("with camera", () => {
+    beforeEach(() => {
+      mockGetUserMedia.mockResolvedValue(new PassThrough());
+    });
+
+    it("renders correctly", () => {
+      const result = render(<App />);
+      const app = result.container.firstChild;
+      expect(app).toHaveClass("App");
+      expect(app).toMatchSnapshot();
+    });
+  });
+
+  describe("without camera", () => {
+    beforeEach(() => {
+      mockGetUserMedia.mockRejectedValue(new DOMException("Permission denied"));
+    });
+
+    it("renders correctly", () => {
+      const result = render(<App />);
+      const app = result.container.firstChild;
+      expect(app).toHaveClass("App");
+      expect(app).toMatchSnapshot();
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
 
-function App() {
+export function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    (async () => {
+      if (videoRef.current) {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+          });
+          videoRef.current.srcObject = stream;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay className="App__video" ref={videoRef} />
     </div>
   );
 }
-
-export default App;
